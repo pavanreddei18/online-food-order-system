07990fb712766a0796eefa82e2fd18102624bc37
pavan_reddei <pavanreddei18@gmail.com>
2025-08-12 19:39:21 +0530

    Update DBCconnection.java

--- src/DBCconnection.java
+++ src/DBCconnection.java
@@ -1,13 +1,15 @@
+package swiggy;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/food_ordering";
+    private static final String URL = "jdbc:mysql://localhost:3306/zomato";
     private static final String USER = "root";
-    private static final String PASS = ""; // Update if you use password
+    private static final String PASS = "root"; // Update if you use password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
     }
 }
+
